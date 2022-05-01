a2f2ecd2c3252a5496f41c8a487ca22ea5ee412c
Asher Norland <asher.norland@gmail.com>
2022-05-01 19:03:39 -0400

    Remove Default Config

--- tests/unit/test_generator.py
+++ tests/unit/test_generator.py
@@ -4,12 +4,23 @@ TODO
 import logging
 
 import pytest
-from cppython.data import default_pyproject
-from cppython_core.schema import GeneratorConfiguration
+from cppython_core.schema import (
+    PEP621,
+    CPPythonData,
+    GeneratorConfiguration,
+    PyProject,
+    TargetEnum,
+    ToolData,
+)
 from pytest_cppython.plugin import GeneratorUnitTests
 from pytest_mock import MockerFixture
 
 from cppython_vcpkg.plugin import VcpkgGenerator
+
+default_pep621 = PEP621(name="test_name", version="1.0")
+default_cppython_data = CPPythonData(**{"target": TargetEnum.EXE})
+default_tool_data = ToolData(**{"cppython": default_cppython_data})
+default_pyproject = PyProject(**{"project": default_pep621, "tool": default_tool_data})
 
 
 class TestCPPythonGenerator(GeneratorUnitTests):
