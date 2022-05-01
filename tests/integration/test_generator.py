6962860041a4a475879d074753546ebe8d29dada
Asher Norland <asher.norland@gmail.com>
2022-05-01 19:03:39 -0400

    Remove Default Config

--- tests/integration/test_generator.py
+++ tests/integration/test_generator.py
@@ -4,11 +4,22 @@ TODO
 import logging
 
 import pytest
-from cppython.data import default_pyproject
-from cppython_core.schema import GeneratorConfiguration
+from cppython_core.schema import (
+    PEP621,
+    CPPythonData,
+    GeneratorConfiguration,
+    PyProject,
+    TargetEnum,
+    ToolData,
+)
 from pytest_cppython.plugin import GeneratorIntegrationTests
 
 from cppython_vcpkg.plugin import VcpkgGenerator
+
+default_pep621 = PEP621(name="test_name", version="1.0")
+default_cppython_data = CPPythonData(**{"target": TargetEnum.EXE})
+default_tool_data = ToolData(**{"cppython": default_cppython_data})
+default_pyproject = PyProject(**{"project": default_pep621, "tool": default_tool_data})
 
 
 class TestCPPythonGenerator(GeneratorIntegrationTests):
